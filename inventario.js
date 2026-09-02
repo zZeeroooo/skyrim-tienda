ef9478b775ece28bb617f2fde1bf4648d769e37e
Cristobal_Varok <cris.zunigac@duocuc.cl>
2026-09-01 23:43:08 -0400

    Se agregan objetos al inventario

--- inventario.js
+++ inventario.js
@@ -153,6 +153,167 @@ const inventario = {
         { id: "col-pluma", name: "Pluma", price: 2 }
       ]
     }
+  ],
+
+  herreria: [
+    {
+      titulo: "Armas",
+      items: [
+        { id: "her-arma-hierro-acero", name: "Hierro / Acero", price: 50 },
+        { id: "her-arma-orco", name: "Orco", price: 100 },
+        { id: "her-arma-enano", name: "Enano", price: 150 },
+        { id: "her-arma-elfico", name: "Élfico", price: 200 },
+        { id: "her-arma-cristal", name: "Cristal", price: 350 },
+        { id: "her-arma-ebano", name: "Ébano", price: 500 }
+      ]
+    },
+    {
+      titulo: "Armaduras Ligeras",
+      items: [
+        { id: "her-lig-piel-cuero", name: "Piel / Cuero", price: 15 },
+        { id: "her-lig-imperial", name: "Imperial ligera", price: 60 },
+        { id: "her-lig-cristal", name: "Cristal", price: 300 },
+        { id: "her-lig-escamas-dragon", name: "Escamas de dragón", price: 450 },
+        { id: "her-lig-hermandad-oscura", name: "Hermandad oscura", price: 80 },
+        { id: "her-lig-nightingale", name: "Nightingale", price: 350 },
+        { id: "her-lig-hermandad-ladrones", name: "Hermandad de Ladrones", price: 40 },
+        { id: "her-lig-linwe", name: "Linwe", price: 60 },
+        { id: "her-lig-gremio-ladrones", name: "Gremio de Ladrones", price: 60 },
+        { id: "her-lig-antiguos-dioses", name: "Antiguos Dioses", price: 90 },
+        { id: "her-lig-penitus-oculatus", name: "Guardia Penitus Oculatus", price: 70 },
+        { id: "her-lig-capas-tormenta", name: "Capas de la tormenta", price: 55 },
+        { id: "her-lig-renegados", name: "Renegados", price: 45 }
+      ]
+    },
+    {
+      titulo: "Armaduras Pesadas",
+      items: [
+        { id: "her-pes-hierro-acero", name: "Hierro / Acero", price: 60 },
+        { id: "her-pes-imperial", name: "Imperial", price: 90 },
+        { id: "her-pes-nordica-antigua", name: "Nórdica Antigua", price: 120 },
+        { id: "her-pes-hueso-dragon", name: "Hueso de dragón", price: 480 },
+        { id: "her-pes-ebano", name: "Ébano", price: 520 },
+        { id: "her-pes-daedrica", name: "Draédrica", price: 600 },
+        { id: "her-pes-blades", name: "Blades", price: 150 },
+        { id: "her-pes-orco", name: "Orco", price: 130 },
+        { id: "her-pes-enano", name: "Enano", price: 170 }
+      ]
+    },
+    {
+      titulo: "Flechas",
+      items: [
+        { id: "her-flecha-falmer", name: "Falmer", price: 1 },
+        { id: "her-flecha-renegados", name: "Renegados", price: 1 },
+        { id: "her-flecha-hierro", name: "Hierro", price: 1 },
+        { id: "her-flecha-nordica-antigua", name: "Nórdica Antigua", price: 2 },
+        { id: "her-flecha-acero", name: "Acero", price: 2 },
+        { id: "her-flecha-enana", name: "Enana", price: 3 },
+        { id: "her-flecha-orca", name: "Orca", price: 4 },
+        { id: "her-flecha-elfica", name: "Élfica", price: 5 },
+        { id: "her-flecha-cristal", name: "Cristal", price: 8 },
+        { id: "her-flecha-ebano", name: "Ébano", price: 10 },
+        { id: "her-flecha-esfera-enana", name: "Esfera enana", price: 6 },
+        { id: "her-flecha-daedrica", name: "Daédrica", price: 15 },
+        { id: "her-flecha-heroe-nordico", name: "Héroe nórdico", price: 6 },
+        { id: "her-flecha-hueso-dragon", name: "Hueso de dragón", price: 12 }
+      ]
+    },
+    {
+      titulo: "Minerales / Lingotes",
+      items: [
+        { id: "her-min-hierro", name: "Mena / Lingote de hierro", price: 8 },
+        { id: "her-min-plata", name: "Mena / Lingote de plata", price: 15 },
+        { id: "her-min-oro", name: "Mena / Lingote de oro", price: 20 },
+        { id: "her-min-cobre", name: "Mena / Lingote de cobre", price: 6 },
+        { id: "her-min-oricalco", name: "Mena / Lingote de oricalco", price: 10 },
+        { id: "her-min-malaquita", name: "Mena de malaquita / Malaquita refinada", price: 12 },
+        { id: "her-min-piedra-lunar", name: "Mena de piedra lunar / Piedra Lunar refinada", price: 15 },
+        { id: "her-min-mercurio", name: "Mena / Lingote de mercurio", price: 8 },
+        { id: "her-min-ebano", name: "Mena / Lingote de ébano", price: 50 },
+        { id: "her-min-azurita", name: "Mena de azurita", price: 10 },
+        { id: "her-min-estalhrim", name: "Mena / Lingote de estalhrim", price: 40 },
+        { id: "her-min-ambar", name: "Mena de ámbar", price: 12 },
+        { id: "her-min-locura", name: "Mena de locura", price: 25 }
+      ]
+    },
+    {
+      titulo: "Legendarias",
+      items: [
+        { id: "her-leg-daedrico", name: "Daédrico", price: 700 },
+        { id: "her-leg-hueso-dragon", name: "Hueso de Dragón", price: 650 },
+        { id: "her-leg-stalhrim", name: "Stalhrim", price: 550 },
+        { id: "her-leg-nordico", name: "Nórdico", price: 300 }
+      ]
+    }
+  ],
+
+  comida: [
+    {
+      titulo: "Bebidas alcohólicas",
+      items: [
+        { id: "com-beb-hidromiel", name: "Hidromiel", price: 10 },
+        { id: "com-beb-hidromiel-espino-negro", name: "Hidromiel de Espino Negro", price: 15 },
+        { id: "com-beb-cerveza", name: "Cerveza", price: 8 },
+        { id: "com-beb-cerveza-negra", name: "Cerveza negra", price: 10 },
+        { id: "com-beb-vino", name: "Vino", price: 12 },
+        { id: "com-beb-vino-tinto", name: "Vino tinto", price: 12 },
+        { id: "com-beb-vino-blanco", name: "Vino blanco", price: 12 },
+        { id: "com-beb-vino-fuego", name: "Vino de fuego", price: 20 },
+        { id: "com-beb-brandy", name: "Brandy", price: 18 }
+      ]
+    },
+    {
+      titulo: "Comida preparada",
+      items: [
+        { id: "com-prep-estofado-manzana-col", name: "Estofado de manzana y col", price: 6 },
+        { id: "com-prep-estofado-ternera", name: "Estofado de ternera", price: 8 },
+        { id: "com-prep-pan", name: "Pan", price: 2 },
+        { id: "com-prep-pan-mantequilla", name: "Pan con mantequilla", price: 3 },
+        { id: "com-prep-queso", name: "Queso", price: 3 },
+        { id: "com-prep-torta", name: "Torta", price: 5 },
+        { id: "com-prep-torta-manzana", name: "Torta de manzana", price: 6 },
+        { id: "com-prep-empanada", name: "Empanada", price: 4 },
+        { id: "com-prep-empanada-manzana", name: "Empanada de manzana", price: 5 },
+        { id: "com-prep-pescado-ahumado", name: "Pescado ahumado", price: 4 },
+        { id: "com-prep-salmon-ahumado", name: "Salmón ahumado", price: 5 }
+      ]
+    },
+    {
+      titulo: "Frutas y verduras",
+      items: [
+        { id: "com-fv-manzana", name: "Manzana", price: 1 },
+        { id: "com-fv-zanahoria", name: "Zanahoria", price: 1 },
+        { id: "com-fv-cebolla", name: "Cebolla", price: 1 },
+        { id: "com-fv-puerro", name: "Puerro", price: 1 },
+        { id: "com-fv-tomate", name: "Tomate", price: 1 }
+      ]
+    },
+    {
+      titulo: "Ingredientes crudos",
+      items: [
+        { id: "com-ing-ajo", name: "Ajo", price: 1 },
+        { id: "com-ing-carne-caballo", name: "Carne de caballo", price: 3 },
+        { id: "com-ing-carne-ciervo", name: "Carne de ciervo", price: 3 },
+        { id: "com-ing-pollo", name: "Pollo", price: 3 },
+        { id: "com-ing-huevo-gallina", name: "Huevo de gallina", price: 1 },
+        { id: "com-ing-leche", name: "Leche", price: 2 },
+        { id: "com-ing-mantequilla", name: "Mantequilla", price: 2 },
+        { id: "com-ing-miel", name: "Miel", price: 3 },
+        { id: "com-ing-sal", name: "Sal", price: 1 },
+        { id: "com-ing-saco-harina", name: "Saco de harina", price: 5 },
+        { id: "com-ing-trigo", name: "Trigo", price: 1 }
+      ]
+    },
+    {
+      titulo: "Varios",
+      items: [
+        { id: "com-var-plato", name: "Plato", price: 1 },
+        { id: "com-var-tazon", name: "Tazón", price: 1 },
+        { id: "com-var-copa", name: "Copa", price: 2 },
+        { id: "com-var-jarra", name: "Jarra", price: 2 },
+        { id: "com-var-botella-vino", name: "Botella de vino", price: 3 }
+      ]
+    }
   ]
 
   // A medida que Cris y Vale terminen sus tiendas, agregan aquí
